04665ff616eae2812d1e3845b1aaf6483afd9848
stephane <fs@email.com>
2023-06-27 20:23:00 +0200

    first commit

---
+++ src/app/views/views.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainViewComponent } from './main-view/main-view.component';
+
+@NgModule({
+  declarations: [MainViewComponent],
+  imports: [CommonModule],
+  exports: [MainViewComponent],
+})
+export class ViewsModule {}
